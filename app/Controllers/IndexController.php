8ec860198bcd06143ce56043d552c2fa50f03a78
fantTom <fant-tom@mail.ru>
2020-02-05 01:49:36 +0300

    basic app

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -7,6 +7,8 @@ use App\Core\Http\Controller;
 class IndexController extends Controller {
     public function indexAction()
     {
+
+        dd($this->entityManager);
         return $this->render('index', ['title' => "Главная страница"]);
     }
 }
--- app/Core/Http/Controller.php
+++ app/Core/Http/Controller.php
@@ -10,14 +10,18 @@ abstract class Controller implements ControllerInterface
 {
 
     protected $view;
+    protected $entityManager;
+
+
+
+    public function __construct()
+    {
+        $this->entityManager = app()->get('orm')->getEntityManager();
+        $this->view = new View();
+    }
 
     public function render($path, $data = [])
     {
         return new Response($this->view->make($path, $data));
-    }
-
-    public function __construct()
-    {
-        $this->view = new View();
     }
 }
